a9c5b0e82ee0d307337c433e02285b6015c9d5fc
Hey-Ismail <Heythereitsfahim@gmail.com>
2026-03-24 22:23:41 +0600

    tailwind file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./style/**/*.{css,scss}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
